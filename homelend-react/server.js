baf5fdc08cc2b7ad4357e880b876f520aa71facb
Diaachenko <dacenkonazar3@gmail.com>
2026-05-10 09:55:26 +0300

    make it work

--- homelend-react/server.js
+++ homelend-react/server.js
@@ -106,7 +106,8 @@ app.post('/api/reviews', authenticate, async (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
-app.get('*', (req, res) => {
+
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
