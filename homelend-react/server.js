b6a2b44b0441d14b9a1a9009700a547af10f6576
Diaachenko <dacenkonazar3@gmail.com>
2026-05-10 22:32:30 +0300

    workkk

--- homelend-react/server.js
+++ homelend-react/server.js
@@ -107,7 +107,7 @@ app.post('/api/reviews', authenticate, async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('/(.*)', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
